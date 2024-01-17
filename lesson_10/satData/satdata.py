2a1007f2a1daed17946f4097857351455824ea19
Adrian <adrian.garcia0239@gmail.com>
2024-01-16 21:32:34 -0600

    Solution

--- lesson_10/satData/satdata.py
+++ lesson_10/satData/satdata.py
@@ -2,41 +2,30 @@ import json
 
 
 class SatData:
-  def __init__(self, json_file):
-    with open(json_file, "r") as file:
-      self._file_data = json.load(file)
-      
-  
-#   def save_as_csv(self, dbns):
-#     with open('dbn.csv', 'w') as file:
-#       for data in self._file_data["data"]:
-#         for list in list:
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      #! Data / View / Columns / 
-      
-      # {
-      #   {
-      #     "data" : [
-      #       8],
-      #       []
-      #     ]
-      #   }
-      # }
-      
-      
-      
-      
-      
-      
-      
-      
+    def __init__(self, json_file):
+        with open(json_file, "r") as file:
+            all_data = json.load(file)
+            formatted_sat_data = {}
+            for row in all_data['data']:
+                formatted_row = ", ".join([row[8], row[9], ("" if str(row[10]) == None else str(row[10])),
+                                           ("" if str(row[11]) ==
+                                            None else str(row[11])),
+                                           ("" if str(row[12]) ==
+                                            None else str(row[12])),
+                                           ("" if str(row[13]) == None else str(row[13]))])
+                formatted_sat_data[row[8]] = formatted_row
+            self._data = formatted_sat_data
+
+    def save_as_csv(self, dbn_list):
+        with open('dbn.csv', 'w') as outfile:
+            outfile.write(
+                'DBN, School Name, Number of Test Takers, Critical Reading Mean, Mathematics Mean, Writing Mean' + '\n')
+            for dbn in dbn_list:
+                if dbn in self._data:
+                    outfile.write(self._data[dbn] + '\n')
+        return
+
+
+sd = SatData('sat.json')
+dbns = ["02M303", "02M294", "01M450", "02M418"]
+sd.save_as_csv(dbns)
